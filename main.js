1f5f731b370c2cbdf85bf2368b9d0aa63eaf1bb2
JMT79 <infomoreno79@gmail.com>
2024-12-04 00:24:21 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+let columnaActiva = 1;
+const columnas = document.querySelectorAll(".columna");
+columnas.forEach((columna, indice) => {
+    columna.addEventListener("click", function(){
+        cambiarColumna(indice)
+    })
+})
+
+function cambiarColumna(indice){
+
+    columnas[columnaActiva].classList.remove("activa")
+    columnas[indice].classList.add("activa")
+    columnaActiva=indice
+}
+
